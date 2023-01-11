13920eb427c0ac18d24853eb478649d9425c225c
Damien <damien.deboudt@sfr.fr>
2023-01-11 16:36:09 +0100

    en cours

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+function maFonctiontrue () {
+    return true;
+}
+
+
+//exo 2
+
+function fonction2 (string $exo2) {
+        return $exo2;
+}
+
+//exo3
+
+function fonction3 (string $exo3, string $exo3bis) {
+    return $exo3 . $exo3bis;
+}
+
+//exo4
